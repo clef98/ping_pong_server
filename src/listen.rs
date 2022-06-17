50b9b0f12113a61ad1356da6e18c16f2d6d1310b
ericsong <ericysong007@gmail.com>
2022-06-17 19:33:55 -0400

    Removed compliation errors for TCP, yet to shutdown connection.

--- src/listen.rs
+++ src/listen.rs
@@ -1,8 +1,8 @@
-use std::io::Read;
-use std::io::prelude::*;
-use std::net::{TcpStream};
+
+use std::net::{Shutdown, TcpStream};
 use std::net::TcpListener;
-use std::str::from_utf8;
+use std::io::{Read, Write, Error};
+//use std::str::from_utf8;
 use std::thread;
 
 //MIGHT NEED SERVER ESTAVLISHED LOCALLY IN SAME PROJECT
@@ -14,7 +14,7 @@ pub fn tcp(address: &str) {
         match stream {
             Ok(stream) => {
                 thread::spawn(move || { handle_connection(stream) });
-            }
+            },
             Err(e) => {
                 println!("Failed to receive data: {}", e);
             }
@@ -24,9 +24,30 @@ pub fn tcp(address: &str) {
 
     fn handle_connection(mut stream: TcpStream) {
         let mut buffer = [0; 1024];
-        //let mut count = 0;
-        stream.read(&mut buffer).unwrap();
-        let response = "ping";
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+        let mut count = 0;
+        'reading_stream: while match stream.read(&mut buffer){
+            Ok(_) => {
+                let message = String::from_utf8_lossy(&buffer[..]);
+                if message.contains("ping") {
+                    stream.write("pong\n".as_bytes()).unwrap();
+                } else if message.contains("pong") {
+                    stream.write("ping\n".as_bytes()).unwrap();
+                } else {
+                    stream.write("Message received.\n".as_bytes()).unwrap();
+                }
+                stream.flush().unwrap();
+                count+=1;
+                if count == 3{
+                    stream.shutdown(Shutdown::Both);
+                    println!("Maximum of three messages permitted on connection. Connection is terminated.");
+                }
+                true
+            },
+            Err(e) => {
+                println!("Failed to process connection: {}", e);
+                break 'reading_stream;
+            }
+        } {
+            println!("Successful ping ping.");
+        }
     }
