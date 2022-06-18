ab519e7bccadc43e05c5ddbbd7b64e3f3a113d43
ericsong <ericysong007@gmail.com>
2022-06-17 20:02:50 -0400

    Adjusted match and Err

--- src/listen.rs
+++ src/listen.rs
@@ -19,47 +19,56 @@ pub fn tcp(address: &str) {
     drop(listener);
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    let mut count = 0;
-    'reading_stream: while match stream.read(&mut buffer) {
-        Ok(_) => {
-            let message = String::from_utf8_lossy(&buffer[..]);
-            if message.contains("ping") {
-                stream.write("pong\n".as_bytes()).unwrap();
-            } else if message.contains("pong") {
-                stream.write("ping\n".as_bytes()).unwrap();
-            } else {
-                stream.write("Message received.\n".as_bytes()).unwrap();
+    fn handle_connection(mut stream: TcpStream) {
+        let mut buffer = [0; 1024];
+        let mut count = 0;
+        'reading_stream: while match stream.read(&mut buffer){
+            Ok(_) => {
+                let message = String::from_utf8_lossy(&buffer[..]);
+                if message.contains("ping") {
+                    stream.write("pong\n".as_bytes()).unwrap();
+                } else if message.contains("pong") {
+                    stream.write("ping\n".as_bytes()).unwrap();
+                } else {
+                    stream.write("Message received.\n".as_bytes()).unwrap();
+                }
+                stream.flush().unwrap();
+                count+=1;
+                if count == 3{
+                    stream.shutdown(Shutdown::Both).unwrap();
+                    println!("Maximum of three messages permitted on connection. Connection is terminated.");
+                    break 'reading_stream;
+                }
+                true
             }
-            stream.flush().unwrap();
-            count += 1;
-            if count == 3 {
+            Err(e) => {
+                println!("Failed to process connection: {}", e);
                 stream.shutdown(Shutdown::Both).unwrap();
-                println!("Maximum of three messages permitted on connection. Connection is terminated.");
                 break 'reading_stream;
             }
-            true
+        } {
+            println!("Successful ping ping.");
+        }
+    }
+
+pub fn udp(address: &str) {
+    println!("Begun UDP connection request.");
+    let socket = UdpSocket::bind(address).expect("Address is not valid.");
+    let mut buffer = [0; 1024];
+    match socket.recv_from(&mut buffer) {
+        Ok(_) => {
+            println!("Connected made");
+            let message = String::from_utf8_lossy(&buffer[..]);
+            if message.contains("ping") {
+                socket.send_to("pong\n".as_bytes(), address).expect("Error with message.");
+            } else if message.contains("pong") {
+                socket.send_to("ping\n".as_bytes(), address).expect("Error with message.");
+            } else {
+                socket.send_to("Message received.\n".as_bytes(), address).expect("Error with messsage.");
+            }
         }
         Err(e) => {
             println!("Failed to process connection: {}", e);
-            stream.shutdown(Shutdown::Both).unwrap();
-            break 'reading_stream;
         }
-    } {
-        println!("Successful ping ping.");
-    }
-}
-
-pub fn udp(address: &str) {
-    let socket = UdpSocket::bind(address).expect("Address is not valid.");
-    let buffer = [0; 1024];
-    let message = String::from_utf8_lossy(&buffer[..]);
-    if message.contains("ping") {
-        socket.send_to("pong\n".as_bytes(), address).expect("Error with message.");
-    } else if message.contains("pong") {
-        socket.send_to("ping\n".as_bytes(), address).expect("Error with message.");
-    } else {
-        socket.send_to("Message received.\n".as_bytes(), address).expect("Error with messsage.");
     }
 }
